0d5f438db8f7de1ae7d5186dd030fd29c2ff83c0
Roxie Gibson <me@roxxers.xyz>
2024-11-10 20:12:31 +0000

    feat: added fetchuser service

---
+++ internal/core/services/user.go
@@ -0,0 +1,23 @@
+package services
+
+import (
+	"github.com/roxxers/surfe-techtest/internal/core/domain"
+	"github.com/roxxers/surfe-techtest/internal/ports"
+)
+
+type Service struct {
+	db ports.Database
+}
+
+func NewService(db ports.Database) *Service {
+	return &Service{db: db}
+}
+
+func (s *Service) FetchUser(userId uint64) *domain.User {
+	user := s.db.GetUser(userId)
+	return &domain.User{
+		Id:        user.Id,
+		Name:      user.Name,
+		CreatedAt: user.CreatedAt,
+	}
+}
