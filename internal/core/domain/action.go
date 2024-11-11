1bfa30c3fe1b9ad221ad22f9d00be652f90c42c5
Roxie Gibson <me@roxxers.xyz>
2024-11-11 01:40:31 +0000

    refactor: move int values from uint to int incase of need for negatives for non-valid IDs

--- internal/core/domain/action.go
+++ internal/core/domain/action.go
@@ -5,9 +5,9 @@ import "time"
 // Defining the json mapping here does couple the adapter with the domain and I'm doing it here to save time
 
 type Action struct {
-	Id           uint64    `json:"id"`
+	Id           int64     `json:"id"`
 	Type         string    `json:"type"` // Possibly Enum here for better type handling when all possible actions are defined
-	UserId       uint64    `json:"userId"`
-	TargetUserId uint64    `json:"targetUser,omitempty"`
+	UserId       int64     `json:"userId"`
+	TargetUserId int64     `json:"targetUser,omitempty"`
 	CreatedAt    time.Time `json:"createdAt"`
 }
--- internal/core/services/user.go
+++ internal/core/services/user.go
@@ -13,7 +13,7 @@ func NewService(db ports.Database) *Service {
 	return &Service{db: db}
 }
 
-func (s *Service) FetchUser(userId uint64) *domain.User {
+func (s *Service) FetchUser(userId int64) *domain.User {
 	user := s.db.GetUser(userId)
 	return &domain.User{
 		Id:        user.Id,
@@ -22,7 +22,7 @@ func (s *Service) FetchUser(userId uint64) *domain.User {
 	}
 }
 
-func (s *Service) GetUserActionCount(userId uint64) int32 {
+func (s *Service) GetUserActionCount(userId int64) int32 {
 	actions := s.db.GetActionsForUser(userId)
 	return int32(len(actions))
 }
