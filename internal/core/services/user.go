c7d6746dc060564f2b54c32f66c927c79c10103e
Roxie Gibson <roxanne.gibson@insurancerevolution.co.uk>
2024-11-11 16:38:11 +0000

    feat: added task 3, service for calculating the probablity of the next action

--- internal/core/services/user.go
+++ internal/core/services/user.go
@@ -1,6 +1,9 @@
 package services
 
 import (
+	"math"
+	"sort"
+
 	"github.com/roxxers/surfe-techtest/internal/core/domain"
 	"github.com/roxxers/surfe-techtest/internal/ports"
 )
@@ -25,6 +28,44 @@ func (s *Service) FetchUser(userId int64) *domain.User {
 func (s *Service) GetUserActionCount(userId int64) int32 {
 	actions := s.db.GetActionsForUser(userId)
 	return int32(len(actions))
+}
+
+// CalculateNextActionProbablity checks all occurances of next action by all users and returns a map of actions and their probablity of being the next action taken by a user.
+// This is done via looking at what the total amount of that action is in proportion to the total number of actions taken after a given action.
+func (s *Service) CalculateNextActionProbablity(actionType string) map[string]float64 {
+	// Since the way the task is worded does not require me to make this function as performant as possible, I will be sorting the lists in the function
+	// In a more optimal world, we would have these sorted at a database level, with either making those calls in the database layer (to save on memory usage here)
+	// or with k-orderable ID's which would allow us to make assuptions when our ID's come through to this side if the database layer is sharded.
+	actionsPerUser := s.db.GetActionsPerUser()
+	nextActions := make(map[string]int)
+	totalActions := 0
+	actionProbablities := make(map[string]float64)
+
+	for _, actions := range actionsPerUser {
+		sort.Slice(actions, func(i, j int) bool {
+			return actions[i].CreatedAt.Before(actions[j].CreatedAt)
+		})
+		for i, action := range actions {
+			if action.Type == actionType {
+				if i == len(actions)-1 {
+					// This is the last action of the user, cannot check the next one
+					continue
+				}
+				if _, ok := nextActions[action.Type]; !ok {
+					nextActions[action.Type] = 0
+				}
+				nextActions[actions[i+1].Type]++
+				totalActions++
+			}
+		}
+	}
+
+	for action, count := range nextActions {
+		probablity := float64(count) / float64(totalActions)
+		// This is to make the end result look like the API Example, but it could lead to the total not equalling 1 which would make the probablities slightly off.
+		actionProbablities[action] = math.Round(probablity*100) / 100
+	}
+	return actionProbablities
 }
 
 // Designing this function keeping in mind complexity, it is only limited to the size of the users and all of their actions,
